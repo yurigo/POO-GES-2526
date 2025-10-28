36d07ea0c37ee0dfa094185b655f4529e891db78
Yuri <ygonzoo@gmail.com>
2025-10-28 12:52:30 +0100

    add SoundWave to imagine pidgeons code sample

--- session 11/Imagine Pidgeons/src/Main.java
+++ session 11/Imagine Pidgeons/src/Main.java
@@ -3,10 +3,25 @@ public class Main {
         SoundSynth synth = new SoundSynth();
 
         try{
-            synth.makeSound(441, 1000);
-            synth.makeSound(440, 200);
-            synth.makeSound(700, 2000);
-            synth.makeSound(200, 1000);
+            synth.makeSound(WaveType.SINE, 440, 1000);
+            synth.makeSound(WaveType.SINE, 440, 200);
+            synth.makeSound(WaveType.SINE, 700, 500);
+            synth.makeSound(WaveType.SINE, 440, 1000);
+
+            synth.makeSound(WaveType.SQUARE, 440, 1000);
+            synth.makeSound(WaveType.SQUARE, 440, 200);
+            synth.makeSound(WaveType.SQUARE, 700, 500);
+            synth.makeSound(WaveType.SQUARE, 440, 1000);
+
+            synth.makeSound(WaveType.TRIANGLE, 440, 1000);
+            synth.makeSound(WaveType.TRIANGLE, 440, 200);
+            synth.makeSound(WaveType.TRIANGLE, 700, 500);
+            synth.makeSound(WaveType.TRIANGLE, 440, 1000);
+
+            synth.makeSound(WaveType.SAWTOOTH, 440, 1000);
+            synth.makeSound(WaveType.SAWTOOTH, 440, 200);
+            synth.makeSound(WaveType.SAWTOOTH, 700, 500);
+            synth.makeSound(WaveType.SAWTOOTH, 440, 1000);
         }
         catch(Exception e){
             System.out.println(e);
